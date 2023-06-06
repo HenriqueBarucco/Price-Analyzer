6aab96a861af40c84c9a5678dd2f9a2440fe2559
Henrique Barucco <henrique_barucco@hotmail.com>
2023-06-06 12:19:16 -0300

    feat(puppeteer.js): adiciona opções de segurança ao lançar o navegador Puppeteer Adiciona as opções "--no-sandbox" e "--disable-setuid-sandbox" ao lançar o navegador Puppeteer para melhorar a segurança do aplicativo. Essas opções desabilitam o uso do sandbox do sistema operacional e do conjunto de permissões do usuário, o que pode reduzir a segurança do aplicativo.

--- src/helpers/puppeteer.js
+++ src/helpers/puppeteer.js
@@ -1,7 +1,10 @@
 import puppeteer from "puppeteer";
 
 export default async function getPrice() {
-    const browser = await puppeteer.launch({ headless: "new" });
+    const browser = await puppeteer.launch({
+        headless: "new",
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
     const page = await browser.newPage();
 
     await page.goto(process.env.URL);
