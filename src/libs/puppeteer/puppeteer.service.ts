7f238f0f9b15a0cd41fafdb7fea91a57cbf3a1e4
Henrique Barucco <henrique_barucco@hotmail.com>
2024-07-31 13:29:15 -0300

    feat: use brightdata to proxy puppeteer

--- src/libs/puppeteer/puppeteer.service.ts
+++ src/libs/puppeteer/puppeteer.service.ts
@@ -1,36 +1,28 @@
 import { Injectable } from '@nestjs/common'
-import puppeteer, { Browser } from 'puppeteer'
+import puppeteer from 'puppeteer-core'
 
 @Injectable()
 export class PuppeteerService {
-  private browser: Browser
-
-  async initBrowser() {
-    this.browser = await puppeteer.launch({
-      headless: true,
-      args: ['--no-sandbox'],
-    })
-  }
-
   async getScreenshot({ url }: { url: string }): Promise<string> {
-    if (!this.browser) {
-      await this.initBrowser()
+    try {
+      const browser = await puppeteer.connect({
+        browserWSEndpoint: process.env.BROWSER_WS_ENDPOINT,
+      })
+
+      const page = await browser.newPage()
+
+      await page.setViewport({ width: 1920, height: 1080 })
+
+      await page.goto(url, { waitUntil: 'load' })
+
+      const screenshot = await page.screenshot()
+
+      await browser.close()
+
+      return screenshot.toString('base64')
+    } catch (error) {
+      console.error(error)
+      throw error
     }
-
-    const page = await this.browser.newPage()
-    await page.setViewport({ width: 1920, height: 1080 })
-
-    await page.goto(url, { waitUntil: 'load' })
-
-    const screenshot = await page.screenshot()
-
-    await this.closeBrowser()
-
-    return screenshot.toString('base64')
-  }
-
-  async closeBrowser() {
-    await this.browser.close()
-    this.browser = null
   }
 }
